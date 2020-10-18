a43ec5a0ddce9fbabe0e1ac9726f9f45df16df48
sunildipun <sunil.6318ie@gmail.com>
2020-10-18 18:07:20 +0530

    search result component inside the search page

--- src/pages/searchPage/searchPage.component.jsx
+++ src/pages/searchPage/searchPage.component.jsx
@@ -3,6 +3,8 @@ import './searchPage.style.scss';
 
 import { Button } from "@material-ui/core";
 
+import  SearchResult from './../../components/searchResult/searchResult.component';
+
 const SearchPage = () => {
     return (
         <div className='searchPage'>
@@ -15,6 +17,16 @@ const SearchPage = () => {
                 <Button variant="outlined">Rooms and beds</Button>
                 <Button variant="outlined">More filters</Button>
             </div>
+
+            <SearchResult
+                img="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ_wbPYTxQPMcBh7SPzLFActXnP3uhifeVT_g&usqp=CAU"
+                location="Private room in center of London"
+                title="Stay at this spacious Edwardian House"
+                description="1 guest · 1 bedroom · 1 bed · 1.5 shared bthrooms · Wifi · Kitchen · Free parking · Washing Machine"
+                star={4.73}
+                price="£30 / night"
+                total="£117 total"
+            />
         </div>
     )
 };
